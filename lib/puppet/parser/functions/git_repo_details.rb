9a3469d06928f799f08c4bb425e0bf6fce610a6c
Jesse Reynolds <jesse@va.com.au>
2015-11-03 17:09:58 +1030

    print ENV

--- lib/puppet/parser/functions/git_repo_details.rb
+++ lib/puppet/parser/functions/git_repo_details.rb
@@ -1,6 +1,6 @@
 module Puppet::Parser::Functions
   newfunction(:git_repo_details, :type => :rvalue) do |args|
-    return "Sausage! #{args.join(', ')} #{`pwd`} #{`env`}"
+    return "Sausage! #{args.join(', ')} #{ENV}"
   end
 end
 
